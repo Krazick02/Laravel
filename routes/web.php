6ccc8b26bf636383b692731f02dd27fd5a9d42a2
unknown <elangelcotaymas@gmail.com>
2022-06-18 23:52:33 -0600

    Optimizacion de las rutas con route resource

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,26 @@ use App\Http\Controllers\CursoController;
 
 Route::get('/',HomeController::class);
 
-Route::controller(CursoController::class)->group(function(){
-    //las rutas get son para mostrar informacion
-    Route::get('cursos','index')->name('cursos.index');
-    Route::get('cursos/create','create')->name('cursos.create');
-    //las rutas post son ingresar informacion
-    Route::post('cursos','store')->name('cursos.store');
-    Route::get('cursos/{curso}','show')->name('cursos.show');
-    Route::get('cursos/{curso}/edit','edit')->name('cursos.edit');
+//Rutas con un grupo
+// Route::controller(CursoController::class)->group(function(){
+//     //las rutas get son para mostrar informacion
+//     Route::get('cursos','index')->name('cursos.index');
+//     Route::get('cursos/create','create')->name('cursos.create');
+//     //las rutas post son ingresar informacion
+//     Route::post('cursos','store')->name('cursos.store');
+//     Route::get('cursos/{curso}','show')->name('cursos.show');
+//     Route::get('cursos/{curso}/edit','edit')->name('cursos.edit');
 
-    //las rutas put son para actualizar un registros
-    Route::put('cursos/{curso}','update')->name('cursos.update');
+//     //las rutas put son para actualizar un registros
+//     Route::put('cursos/{curso}','update')->name('cursos.update');
 
-    //las rutas delete son para eliminar elementos
-    Route::delete('cursos/{curso}','destroy')->name('cursos.destroy');
-});
+//     //las rutas delete son para eliminar elementos
+//     Route::delete('cursos/{curso}','destroy')->name('cursos.destroy');
+// });
+
+
+//Asi definir las rutas con el metodo resource, necesita ser elaborado con las convenciones
+//Route::resource('cursos',CursoController::class);
+
+//Asi se pueden modificar el nombre de la ruta, pero sin modificar los nombres en las rutas ni los parametros
+Route::resource('asignaturas',CursoController::class)->parameters(['asignaturas'=>'curso'])->names('cursos');
